86c9c685fc56ec9071a7c4614a5272496e2bb087
Ahmad Faisol <mzfais@gmail.com>
2023-04-27 10:36:16 +0700

    add navbar component

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./navbar.css";
+import Logo from "../../assets/images/logo.png";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import MyButton from "./../Button";
+
+function Header({ navbars }) {
+  const btnOptions = { className: "sign-in-btn", text: "Masuk/Daftar" };
+  return (
+    <Navbar className="navbar-app p-3" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img className="logo" src={Logo} alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto justify-content-end flex-grow-1 pe-3">
+            {navbars.map((item, idx) => {
+              return (
+                <Nav.Link className="mr-3" key={idx} href={item.url}>
+                  {item.page}
+                </Nav.Link>
+              );
+            })}
+          </Nav>
+          <MyButton options={btnOptions} />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
