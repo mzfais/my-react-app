b05ea29846cd565ec808f82993730098d6402a6d
Ahmad Faisol <mzfais@gmail.com>
2023-04-27 16:07:49 +0700

    move products to data

---
+++ src/data/Products.js
@@ -0,0 +1,40 @@
+const Products = [
+    {
+      image:
+        "https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVhY3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+      title: "Bootcamp React Js",
+      tanggal: "20 Februari 2023",
+      mentor: "Faisol",
+      hg_asli: "Rp. 500.000",
+      hg_promo: "Rp. 249.000",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1653387319597-84bde7e5368e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      title: "Bootcamp Vue Js",
+      tanggal: "15 April 2023",
+      mentor: "Zidane",
+      hg_asli: "Rp. 750.000",
+      hg_promo: "Rp. 449.000",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1643116774075-acc00caa9a7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      title: "Bootcamp Next Js",
+      tanggal: "18 Mei 2023",
+      mentor: "Ayra",
+      hg_asli: "Rp. 1.000.000",
+      hg_promo: "Rp. 699.000",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1545670723-196ed0954986?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80",
+      title: "Bootcamp Full Stack Dev.",
+      tanggal: "2 Juni 2023",
+      mentor: "Yunita",
+      hg_asli: "Rp. 2.000.000",
+      hg_promo: "Rp. 999.000",
+    },
+  ];
+
+export default Products;
